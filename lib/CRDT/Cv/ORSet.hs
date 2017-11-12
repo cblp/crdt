e0c4cba348ea405b9366b232d43135fa5372a824
kemphack <kemphack@mail.ru>
2017-11-12 22:35:49 +0300

    glob remake

--- lib/CRDT/Cv/ORSet.hs
+++ lib/CRDT/Cv/ORSet.hs
@@ -12,17 +12,26 @@ import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import           Data.Semigroup (Semigroup ((<>)))
 import           Data.Semilattice (Semilattice)
-import           Data.Set (Set)
-import qualified Data.Set as Set
+import           Numeric.Natural(Natural)
 
 import           CRDT.LamportClock (Pid)
 
-newtype ORSet a = ORSet (Map Pid (Set a))
+type Tag = Natural
+
+newtype ORSet a = ORSet (Map Pid (Map a (Tag, Bool)))
     deriving (Eq, Show)
 
 instance Ord a => Semigroup (ORSet a) where
     ORSet map1 <> ORSet map2 =
-        ORSet (Map.unionWith Set.union map1 map2)
+        ORSet (Map.unionWith (Map.unionWith lww) map1 map2)
+      where
+        lww a b =
+            let (f1, s1) = a
+                (f2, s2) = b in
+            case compare f1 f2 of
+                EQ -> (f1, s1 && s2)
+                LT -> a
+                GT -> b
 
 instance Ord a => Semilattice (ORSet a)
 
@@ -31,12 +40,26 @@ initial = ORSet Map.empty
 
 add :: Ord a => Pid -> a -> ORSet a -> ORSet a
 add pid e (ORSet storage) =
-    ORSet (Map.insertWith Set.union pid (Set.singleton e) storage)
+    ORSet (Map.alter add1 pid storage)
+  where
+    lww a =
+        if snd a then
+            a
+        else
+            (fst a + 1, True)
+    add1 x = Just (maybe (Map.singleton e (0, True)) (Map.alter add2 e) x)
+    add2 x = Just (maybe (0, True) lww x)
 
 remove :: Ord a => Pid -> a -> ORSet a -> ORSet a
 remove pid e (ORSet storage) =
-    ORSet (Map.insertWith (Set.\\) pid (Set.singleton e) storage)
+    ORSet (Map.adjust (Map.adjust lww e) pid storage)
+  where
+    lww a =
+        if snd a then
+            (fst a, False)
+        else
+            a
 
 lookup :: Ord a => a -> ORSet a -> Bool
 lookup e (ORSet storage) =
-    Map.foldr (\a b -> b || Set.member e a) False storage
+    Map.foldr (\a b -> maybe False snd Map.lookup e a || b) False storage
--- test/ORSet.hs
+++ test/ORSet.hs
@@ -9,6 +9,7 @@ import           Test.Tasty.QuickCheck (property)
 
 import           CRDT.Cv.ORSet (ORSet (..))
 import qualified CRDT.Cv.ORSet as ORSet
+import           Data.Semigroup (Semigroup ((<>)))
 
 import           Laws (cvrdtLaws)
 
@@ -16,3 +17,6 @@ test_Cv = cvrdtLaws @(ORSet Int) Nothing
 
 prop_insert = property $ \pid i ->
     not . ORSet.lookup i $ ORSet.remove pid i (ORSet.add pid i (ORSet.initial :: ORSet Int))
+
+prop_add_merge = property $ \(s0 :: ORSet Int) s2 x pid ->
+    let s1 = ORSet.add pid x s0; in ORSet.lookup x (s1 <> s2)
