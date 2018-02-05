9f7b4a06fa3da5b94c418b384721233c171b6ab7
Yuriy Syrovetskiy <cblp@cblp.su>
2018-02-05 12:44:30 +0300

    Let Lamport simulator be a monad transformer
    It is needed to compose it with QuickCheck.GenT

--- lib/CRDT/LamportClock/Simulation.hs
+++ lib/CRDT/LamportClock/Simulation.hs
@@ -6,17 +6,22 @@
 module CRDT.LamportClock.Simulation
     (
     -- * Lamport clock simulation
-      LamportClockSim (..)
-    , ProcessSim (..)
+      LamportClockSim
+    , LamportClockSimT (..)
+    , ProcessSim
+    , ProcessSimT (..)
     , runLamportClockSim
+    , runLamportClockSimT
     , runProcessSim
+    , runProcessSimT
     ) where
 
 import           Control.Monad.Reader (ReaderT, ask, runReaderT)
-import           Control.Monad.RWS.Strict (RWS, evalRWS)
+import           Control.Monad.RWS.Strict (RWST, evalRWS, evalRWST)
 import           Control.Monad.State.Strict (MonadState, get, gets, modify, put,
                                              state)
-import           Control.Monad.Trans (lift)
+import           Control.Monad.Trans (MonadTrans, lift)
+import           Data.Functor.Identity (Identity)
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 
@@ -25,9 +30,14 @@ import           CRDT.LamportClock (Clock, LamportTime (LamportTime), LocalTime,
 
 -- | Lamport clock simulation. Key is 'Pid'.
 -- Non-present value is equivalent to (0, initial).
-newtype LamportClockSim s a =
-    LamportClockSim (RWS s () (Map Pid (ProcessState s)) a)
+newtype LamportClockSimT s m a =
+    LamportClockSim ((RWST s () (Map Pid (ProcessState s)) m) a)
     deriving (Applicative, Functor, Monad)
+
+type LamportClockSim s = LamportClockSimT s Identity
+
+instance MonadTrans (LamportClockSimT s) where
+    lift = LamportClockSim . lift
 
 data ProcessState s = ProcessState
     { time :: LocalTime
@@ -35,13 +45,18 @@ data ProcessState s = ProcessState
     }
 
 -- | ProcessSim inside Lamport clock simulation.
-newtype ProcessSim s a = ProcessSim (ReaderT Pid (LamportClockSim s) a)
+newtype ProcessSimT s m a = ProcessSim (ReaderT Pid (LamportClockSimT s m) a)
     deriving (Applicative, Functor, Monad)
 
-instance Process (ProcessSim s) where
+type ProcessSim s = ProcessSimT s Identity
+
+instance MonadTrans (ProcessSimT s) where
+    lift = ProcessSim . lift . lift
+
+instance Monad m => Process (ProcessSimT s m) where
     getPid = ProcessSim ask
 
-instance Clock (ProcessSim s) where
+instance Monad m => Clock (ProcessSimT s m) where
     getTime = ProcessSim $ do
         pid <- ask
         time <- lift $ preIncrementTime pid
@@ -57,7 +72,7 @@ instance Clock (ProcessSim s) where
         advancePS _       (Just ProcessState{time = current, var}) =
             ProcessState{time = max time current, var}
 
-instance MonadState s (ProcessSim s) where
+instance Monad m => MonadState s (ProcessSimT s m) where
     get = ProcessSim $ do
         pid <- ask
         lift . LamportClockSim $ do
@@ -74,10 +89,17 @@ runLamportClockSim :: s -> LamportClockSim s a -> a
 runLamportClockSim initial (LamportClockSim action) =
     fst $ evalRWS action initial mempty
 
+runLamportClockSimT :: Monad m => s -> LamportClockSimT s m a -> m a
+runLamportClockSimT initial (LamportClockSim action) =
+    fst <$> evalRWST action initial mempty
+
 runProcessSim :: Pid -> ProcessSim s a -> LamportClockSim s a
 runProcessSim pid (ProcessSim action) = runReaderT action pid
 
-preIncrementTime :: Pid -> LamportClockSim s LocalTime
+runProcessSimT :: Pid -> ProcessSimT s m a -> LamportClockSimT s m a
+runProcessSimT pid (ProcessSim action) = runReaderT action pid
+
+preIncrementTime :: Monad m => Pid -> LamportClockSimT s m LocalTime
 preIncrementTime pid = LamportClockSim $ do
     initial <- ask
     state $ \pss -> let
